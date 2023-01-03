e44b8ab2cf400f3552f2968dffa66fefb53ea372
ortalgabay <108272041+ortalgabay@users.noreply.github.com>
2023-01-03 10:46:31 +0200

    Update zeh.tf

--- zeh.tf
+++ zeh.tf
@@ -18,7 +18,8 @@ sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
 export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
-export AWS_SECRET_ACCESS_KEY=zehavit33
+export AWS_SECRET_ACCESS_KEY=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
+export BLA=zehavit1674
 export AWS_DEFAULT_REGION=us-west-2
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
